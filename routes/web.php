eb2b278f91e73767ac668eb156b4c2b92ff85778
Ruii1112 <ruiiiii1112@gmail.com>
2023-11-16 16:30:34 +0900

    curriculum803

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
